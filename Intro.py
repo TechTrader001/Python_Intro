f50949f54885c81b815ae6104ccb6502ee1db9f3
Azeez Yusuf <azeezyusuf.techtrader@gmail.com>
2025-08-13 00:51:09 +0100

    Python intro commit

---
+++ Intro.py
@@ -0,0 +1,42 @@
+#Variable
+
+Student_Name = "Tech_Trader"
+Student_ID = 100
+Student_Age = 70
+Fees_Clearance = True
+
+print (Student_Name, Student_ID, Student_Age, Fees_Clearance)
+
+#List
+
+languages = [1994, "Python", "Java", "HTML"]
+print(languages)
+
+shopping_list = ["Milk", "Banana", "Foodstuff"]
+print(shopping_list)
+
+#Tuples
+
+product = ('XBox', 499.9, "Habibi", 23)
+print(product)
+
+#Set
+
+set_A = {1,2,3,4,5}
+set_B = {4,5,6,7,8}
+print(set_A)
+print(set_B)
+
+#Dictionaries
+
+my_info = {"Name": "TechTrader_001", "Skills": "Tech", "Country": "Nigeria"}
+print(my_info)
+
+#Logical Operator
+
+a = 10
+b = 20
+c = 30
+print(a<b and b<c)
+print(a>b or b>c)
+print(not(a<b and b<c))
